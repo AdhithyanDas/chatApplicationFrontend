b816f585ffe99ed3b483819fe414eefc2274932c
AdhithyanDas <adhithyandask@gmail.com>
2024-12-28 16:39:56 +0530

    completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
-import daisyui from 'daisyui';
-
-export default {
+module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [require("daisyui")],
 };
